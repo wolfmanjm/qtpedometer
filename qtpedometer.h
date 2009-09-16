7eba36a56189a05ff75e9cb81e20200f977c450f
Jim Morris <morris@wolfman.com>
2009-09-15 22:34:19 -0700

    Clean out all old code

--- qtpedometer.h
+++ qtpedometer.h
@@ -1,25 +1,19 @@
-#ifndef QTGPS_H
-#define QTGPS_H
+#ifndef QTPEDOMETER_H
+#define QTPEDOMETER_H
 
 #include <QMutex>
 #include <QTimer>
 
-#include "ui_qtgps.h"
-#include "skyView.h"
-#include "libgpsmm.h"
+#include "ui_qtpedometer.h"
 
-class QtGps : public QWidget
+class QtPedometer : public QWidget
 {
 	Q_OBJECT
 
 	public:
-		QtGps(QWidget *parent = 0, Qt::WFlags f = 0);
-		virtual ~QtGps();
-		
-		void setGpsData(struct gps_data_t *gpsdata, char *message, size_t len= 0, int level= 0);
+		QtPedometer(QWidget *parent = 0, Qt::WFlags f = 0);
+		virtual ~QtPedometer();
 
-	public slots:
-		void newGpsData(struct gps_data_t *gpsdata, char *message);
 		void init();
 
 	protected:
@@ -27,14 +21,10 @@ class QtGps : public QWidget
 		void showEvent(QShowEvent *);
 		void hideEvent(QHideEvent *);
 		void closeEvent (QCloseEvent *) ;
-		
+
 	private:
 		Ui::MainWindow ui;
 		QMutex mutex;
-
-		SkyView *skyView;
-		struct gps_data_t gpsdata;
-		gpsmm gps_rec;
 
 		QString sentence;
 		time_t timer;	/* time of last state change */
@@ -42,18 +32,4 @@ class QtGps : public QWidget
 		bool hidden;
 };
 
-// Singleton for sending signal from a callback
-class ExtSig : public QObject
-{
-	Q_OBJECT
-	public:
-		static ExtSig *self();
-		void send(struct gps_data_t* p, char* buf);
-
-	signals:
-		void sendit(struct gps_data_t* p, char* buf);
-
-	private:
-		ExtSig();
-};
 #endif
